ef73ef7f4583e36bc0ab9888b956cda6651b1821
Marcus-44 <mlubert44@gmail.com>
2024-09-26 14:06:28 -0400

    got card working

--- src/my-card.js
+++ src/my-card.js
@@ -22,11 +22,45 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      
+    .wrapper{
+      width: 500px;
+      border: 3px solid black;
+      text-align: center;
+      background-color: turquoise;
+      overflow: hidden;
+      border-radius: 20px;
+    }
+
+  img {
+    width: 60%;
+    margin: 10px 50px;
+  }
+
+button {
+  padding: 10px;
+  font-size: 20px;
+  background-color: white;
+}
+
+
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    
+    <div>${this.title}</div>
+    
+  <div class="wrapper">
+      <h1 class="cardTitle">USA</h1>
+      <img src="https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg" class="image">
+      <h4>This is an image of the American flag</h4>
+      <button> <a href="https://hax.psu.edu"> Details </a></button>
+  </div>
+
+    `;
   }
 
   static get properties() {
